f1adbdb5560c3dcd9f449a5d094264e31fa9b15f
puglid <puglid@usi.ch>
2020-01-06 16:50:24 +0100

    First Commit

---
+++ MyDashboard/MyDashboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDashboard
+//
+//  Created by Damiano Pugliesi on 06/01/2020.
+//  Copyright © 2020 Damiano Pugliesi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
